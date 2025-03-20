96f29caeed56fa70b14396d3d9dd7d794eff0b35
amytis25 <126446123+amytis25@users.noreply.github.com>
2025-03-20 10:03:08 -0700

    start

---
+++ index.js
@@ -0,0 +1,26 @@
+let header;
+let homePage;
+let about;
+let contact;
+let portfolio;
+
+function goToAbout(){
+
+
+}
+function goToContact(){
+
+    
+}
+function goToPortfolio(){
+
+    
+}
+function goToHome(){
+
+    
+}
+function goToResume(){
+
+    
+}
