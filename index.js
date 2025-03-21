8fe89417bb38f7aa272246e0e3e3ded1b70a2e1a
amytis25 <126446123+amytis25@users.noreply.github.com>
2025-03-20 20:39:30 -0700

    working
    missing some pages

--- index.js
+++ index.js
@@ -1,26 +1,134 @@
-let header;
-let homePage;
-let about;
-let contact;
-let portfolio;
-
-function goToAbout(){
 
 
+function goToHome() {
+    document.getElementById('HomePage').style.display = 'block';
+    document.getElementById('About').style.display = 'none';
+    document.getElementById('Contact').style.display = 'none';
+    document.getElementById('Portfolio').style.display = 'none';
+    document.getElementById('Projects').style.display = 'none';
+    document.getElementById('softwareProjects').style.display = 'none';
+    document.getElementById('hardwareProjects').style.display = 'none';
+    document.getElementById('Art').style.display = 'none';
+    document.getElementById('Photography').style.display = 'none';
+    document.getElementById('resume').style.display = 'none';
 }
-function goToContact(){
 
-    
+function goToAbout() {
+    document.getElementById('HomePage').style.display = 'none';
+    document.getElementById('About').style.display = 'block';
+    document.getElementById('Contact').style.display = 'none';
+    document.getElementById('Portfolio').style.display = 'none';
+    document.getElementById('Projects').style.display = 'none';
+    document.getElementById('softwareProjects').style.display = 'none';
+    document.getElementById('hardwareProjects').style.display = 'none';
+    document.getElementById('Art').style.display = 'none';
+    document.getElementById('Photography').style.display = 'none';
+    document.getElementById('resume').style.display = 'none';
 }
-function goToPortfolio(){
 
-    
+function goToContact() {
+    document.getElementById('HomePage').style.display = 'none';
+    document.getElementById('About').style.display = 'none';
+    document.getElementById('Contact').style.display = 'block';
+    document.getElementById('Portfolio').style.display = 'none';
+    document.getElementById('Projects').style.display = 'none';
+    document.getElementById('softwareProjects').style.display = 'none';
+    document.getElementById('hardwareProjects').style.display = 'none';
+    document.getElementById('Art').style.display = 'none';
+    document.getElementById('Photography').style.display = 'none';
+    document.getElementById('resume').style.display = 'none';
 }
-function goToHome(){
 
-    
+function goToPortfolio() {
+    document.getElementById('HomePage').style.display = 'none';
+    document.getElementById('About').style.display = 'none';
+    document.getElementById('Contact').style.display = 'none';
+    document.getElementById('Portfolio').style.display = 'block';
+    document.getElementById('Projects').style.display = 'none';
+    document.getElementById('softwareProjects').style.display = 'none';
+    document.getElementById('hardwareProjects').style.display = 'none';
+    document.getElementById('Art').style.display = 'none';
+    document.getElementById('Photography').style.display = 'none';
+    document.getElementById('resume').style.display = 'none';
 }
-function goToResume(){
 
+function goToProjects() {
+    document.getElementById('HomePage').style.display = 'none';
+    document.getElementById('About').style.display = 'none';
+    document.getElementById('Contact').style.display = 'none';
+    document.getElementById('Portfolio').style.display = 'none';
+    document.getElementById('Projects').style.display = 'block';
+    document.getElementById('softwareProjects').style.display = 'none';
+    document.getElementById('hardwareProjects').style.display = 'none';
+    document.getElementById('Art').style.display = 'none';
+    document.getElementById('Photography').style.display = 'none';
+    document.getElementById('resume').style.display = 'none';
+}
+
+function goToArt() {
+    document.getElementById('HomePage').style.display = 'none';
+    document.getElementById('About').style.display = 'none';
+    document.getElementById('Contact').style.display = 'none';
+    document.getElementById('Portfolio').style.display = 'none';
+    document.getElementById('Projects').style.display = 'none';
+    document.getElementById('softwareProjects').style.display = 'none';
+    document.getElementById('hardwareProjects').style.display = 'none';
+    document.getElementById('Art').style.display = 'block';
+    document.getElementById('Photography').style.display = 'none';
+    document.getElementById('resume').style.display = 'none';
+}
+
+function goToPhotography() {
+    document.getElementById('HomePage').style.display = 'none';
+    document.getElementById('About').style.display = 'none';
+    document.getElementById('Contact').style.display = 'none';
+    document.getElementById('Portfolio').style.display = 'none';
+    document.getElementById('Projects').style.display = 'none';
+    document.getElementById('softwareProjects').style.display = 'none';
+    document.getElementById('hardwareProjects').style.display = 'none';
+    document.getElementById('Art').style.display = 'none';
+    document.getElementById('Photography').style.display = 'block';
+    document.getElementById('resume').style.display = 'none';
+}
+
+function goToResume() {
+    document.getElementById('HomePage').style.display = 'none';
+    document.getElementById('About').style.display = 'none';
+    document.getElementById('Contact').style.display = 'none';
+    document.getElementById('Portfolio').style.display = 'none';
+    document.getElementById('Projects').style.display = 'none';
+    document.getElementById('softwareProjects').style.display = 'none';
+    document.getElementById('hardwareProjects').style.display = 'none';
+    document.getElementById('Art').style.display = 'none';
+    document.getElementById('Photography').style.display = 'none';
+    document.getElementById('resume').style.display = 'block';
+}
+
+function goToSoftwareProjects() {
+    document.getElementById('HomePage').style.display = 'none';
+    document.getElementById('About').style.display = 'none';
+    document.getElementById('Contact').style.display = 'none';
+    document.getElementById('Portfolio').style.display = 'none';
+    document.getElementById('Projects').style.display = 'none';
+    document.getElementById('softwareProjects').style.display = 'block';
+    document.getElementById('hardwareProjects').style.display = 'none';
+    document.getElementById('Art').style.display = 'none';
+    document.getElementById('Photography').style.display = 'none';
+    document.getElementById('resume').style.display = 'none';
+}
+
+function goToHardwareProjects() {
+    document.getElementById('HomePage').style.display = 'none';
+    document.getElementById('About').style.display = 'none';
+    document.getElementById('Contact').style.display = 'none';
+    document.getElementById('Portfolio').style.display = 'none';
+    document.getElementById('Projects').style.display = 'none';
+    document.getElementById('softwareProjects').style.display = 'none';
+    document.getElementById('hardwareProjects').style.display = 'block';
+    document.getElementById('Art').style.display = 'none';
+    document.getElementById('Photography').style.display = 'none';
+    document.getElementById('resume').style.display = 'none';
+}
+function submitMessage(){
     
 }
